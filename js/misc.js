6910a74edfb84960dd0175cf4d664be4b99d470a
jmjuanes <josemijuanes@gmail.com>
2016-10-12 20:27:01 +0200

    js/misc.js: get chromosomes, regions and selected regions

--- js/misc.js
+++ js/misc.js
@@ -3,7 +3,44 @@ jviz.modules.ideogram.prototype.foot = function(text)
 {
   //Check for undefined text
   if(typeof text === 'undefined'){ var text = ' '; }
-  
+
   //Set the foot content
   this._panel.footContent(text);
+};
+
+//Get chromosomes
+jviz.modules.ideogram.prototype.getChromosomes = function()
+{
+  //Return the chromosomes list
+  return this._genome.chromosomes;
+};
+
+//Get regions by chromosome
+jviz.modules.ideogram.prototype.getRegions = function(chr)
+{
+  //Check for undefined chromosome
+  if(typeof chr === 'undefined'){ return jviz.console.error('Undefined chromosome name', []); }
+
+  //Check for unknown chromosome
+  if(this._regions.list[chr] === 'undefined'){ return []; }
+
+  //Return the regions for this chromosome
+  return this._regions.list[chr];
+};
+
+//Get marked regions by chromosome
+jviz.modules.ideogram.prototype.getMarks = function(chr)
+{
+  //Get the regions for this chromosomes
+  var regions = this.getRegions(chr);
+
+  //Filter the regions list
+  regions.filter(function(el)
+  {
+    //Filter the regions
+    return (el.mark === true) ? true : false;
+  });
+
+  //Return the parsed list
+  return regions;
 };
