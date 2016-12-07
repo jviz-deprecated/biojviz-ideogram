0471adfe1f97828484e05a74361bdf0225139fad
jmjuanes <josemijuanes@gmail.com>
2016-12-07 12:50:51 +0100

    js/menu.js: use panel buttons builder

--- js/menu.js
+++ js/menu.js
@@ -2,22 +2,28 @@
 jviz.modules.ideogram.prototype.menu = function()
 {
   //Get the menu container id
-  var id = this._panel.head().content.id;
+  //var id = this._panel.head().content.id;
 
   //Reset the menu content
-  jviz.dom.empty(id);
+  //jviz.dom.empty(id);
 
   //Add the table button
-  jviz.dom.append(id, { id: this._menu.table.id, class: this._menu.table.class, _html: this._menu.table.text });
+  //jviz.dom.append(id, { id: this._menu.table.id, class: this._menu.table.class, _html: this._menu.table.text });
 
   //Add the background image
   //jviz.dom.style(id, 'background-image', this._menu.table.img);
 
   //Add the preview button
-  jviz.dom.append(id, { id: this._menu.preview.id, class: this._menu.preview.class, _html: this._menu.preview.text });
+  //jviz.dom.append(id, { id: this._menu.preview.id, class: this._menu.preview.class, _html: this._menu.preview.text });
 
   //Add the background image
   //jviz.dom.style(id, 'background-image', this._menu.preview.img);
+
+  //Add the table button
+  this._panel.addBtn(this._menu.table);
+
+  //Add the preview button
+  this._panel.addBtn(this._menu.preview);
 
   //Save this
   var self = this;
