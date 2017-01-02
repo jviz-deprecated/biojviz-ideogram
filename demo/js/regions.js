1b5ab7a63f26a202afa648aeb7ad39e662e10e7b
jmjuanes <josemijuanes@gmail.com>
2017-01-02 10:35:41 +0100

    demo/js/regions.js: added test regions

---
+++ demo/js/regions.js
@@ -0,0 +1,11 @@
+//Initialize the test regions
+var test_regions = [];
+
+//Add test regions
+test_regions.push({ chromosome: '1', start: 11845761, end: 11866185, name: 'MTHFR' });
+test_regions.push({ chromosome: '1', start: 24122063, end: 24127319, name: 'GALE' });
+test_regions.push({ chromosome: '1', start: 24128341, end: 24151974, name: 'HMGCL' });
+test_regions.push({ chromosome: '1', start: 43391020, end: 43424872, name: 'SLC2A1' });
+test_regions.push({ chromosome: '1', start: 45965830, end: 45976764, name: 'MMACHC' });
+test_regions.push({ chromosome: '1', start: 53662075, end: 53679894, name: 'CPT2' });
+test_regions.push({ chromosome: '1', start: 64058921, end: 64125941, name: 'PGM1' });
