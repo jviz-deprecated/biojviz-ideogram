f39beaab8d48a520bdc079ffe184c29a1128df65
jmjuanes <josemijuanes@gmail.com>
2017-01-03 11:27:02 +0100

    js/events.js: updated resize event

--- js/events.js
+++ js/events.js
@@ -2,7 +2,7 @@
 jviz.modules.ideogram.prototype.events = function()
 {
   //Get the last layer ID
-  var id = this._canvas.layerID(this._layers - 1);
+  var id = this._canvas.el.layerID(this._canvas.layers - 1);
 
   //Save this
   var self = this;
@@ -14,7 +14,7 @@ jviz.modules.ideogram.prototype.events = function()
     if(self.loading() === true){ return; }
 
     //Check the chromosome
-    (self._chromosome.now === -1) ? self.previewClick(x,y) : self.chromosomeClick(x,y);
+    (self._chromosome.now === -1) ? self.karyotypeClick(x,y) : self.chromosomeClick(x,y);
   });
 
   //Add the mouse over event
@@ -24,7 +24,7 @@ jviz.modules.ideogram.prototype.events = function()
     if(self.loading() === true){ return; }
 
     //Check the chromosome
-    (self._chromosome.now === -1) ? self.previewMove(x,y) : self.chromosomeMove(x,y);
+    (self._chromosome.now === -1) ? self.karyotypeMove(x,y) : self.chromosomeMove(x,y);
   });
 
   //Add the mouse leave event
@@ -34,16 +34,23 @@ jviz.modules.ideogram.prototype.events = function()
     if(self.loading() === true){ return; }
 
     //Check the chromosome
-    (self._chromosome.now === -1) ? self.previewLeave() : self.chromosomeLeave();
+    (self._chromosome.now === -1) ? self.karyotypeLeave() : self.chromosomeLeave();
   });
 
   //Add the resize event
   $(window).resize(function()
   {
-    //Resize the canvas element
-    self._canvas.resize();
-
-    //Draw again
-    self.draw(self._chromosome.now);
+    //Resize the ideogram
+    self.resize();
   });
+};
+
+//Register events
+jviz.modules.ideogram.prototype.on = function(name, listener)
+{
+  //Register the event
+  this._events.add(name, listener);
+
+  //Continue
+  return this;
 };
